446e62523161e0f61ccea177a267eda277a62428
marcelof0nseca <maf@cesar.school>
2026-02-16 19:09:26 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, "recipes/home.html", context={"nome": "Marcelo"})
-
-
-def contato(request):
-    return render(request, "recipes/contato.html")
-
-
-def sobre(request):
-    return HttpResponse("Olá, esta é a página sobre!")
