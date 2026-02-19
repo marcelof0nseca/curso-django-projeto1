2ca617fd6be447bf7e2d769895a679d48a135ec6
marcelof0nseca <maf@cesar.school>
2026-02-19 16:48:18 -0300

    separando os templates de home e recipe

--- projeto/settings.py
+++ projeto/settings.py
@@ -120,14 +120,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_URL = "static/"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "base_static",
-]
-
-
-STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
